6c8f6f7e34a904465a9c8a7712cedd4882cf0e3e
Mastayxz <gustibagus826@gmail.com>
2025-08-02 19:47:45 +0800

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello masta World!';
+    return 'Hello masta World! yooo';
   }
 }
